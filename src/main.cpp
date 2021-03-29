bb8b1f4c660b7e6163bcca0112414d34da2a095f
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-29 19:50:35 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Ships battle" << std::endl;
-    return 0;
+  std::cout << "Ships battle" << std::endl;
+  bool tiny[] = {true};
+  bool small[] = {true, true};
+  bool medium[] = {true, true, true};
+  bool large[] = {true, true, true, true};
+
+  std::cout << "size tiny " << sizeof(tiny) << std::endl;
+  std::cout << "size small " << sizeof(small) << std::endl;
+  std::cout << "size medium " << sizeof(medium) << std::endl;
+  std::cout << "size large " << sizeof(large) << std::endl;
+
+  return 0;
 }
-
-// Задача 1. Корабли в “морском бое”
-
-// Требуется объявить массивы для игры в морской бой и вывести
-// их размеры в консоль, не используя констант с размерами. 
-// Всего таких типов 4: крошечный, малый, средний и большой. 
-// Для простоты, типом элементов корабля могут быть обычные
-// bool (размер 1 байт).d
